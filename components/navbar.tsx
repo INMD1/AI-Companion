36959e4d86b3fd38d661d0efde22f9ad507e4ead
INMD1 <87979171+INMD1@users.noreply.github.com>
2023-12-20 22:20:15 +0900

    오늘 작업 끝

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,6 +6,7 @@ import { Menu, Sparkle } from "lucide-react"
 import { Poppins } from "next/font/google"
 import Link from "next/link"
 import { Button } from "./ui/button"
+import { ModeToggle } from "./theme-toggle"
 
 const font = Poppins({
     weight: "600",
@@ -13,7 +14,7 @@ const font = Poppins({
 })
 export const Navbar = () => {
     return(
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
                 <Menu className="block md:hidden"/>
                 <Link href="/">
@@ -23,10 +24,11 @@ export const Navbar = () => {
                     </Link>
             </div>
             <div className="flex items-center gap-x-3">
-                <Button size="sm">
+                <Button variant="premium" size="sm">
                     Upgrade
                     <Sparkle className="h-4 w-4 text-white ml-2"/>
                 </Button>
+                <ModeToggle/>
                 <UserButton/>
             </div>
         </div>
